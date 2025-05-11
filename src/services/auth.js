0eed01f32b11db6992270ab620d3a17b6be45ce9
Olexander <dneprovsky1997@icloud.com>
2025-05-12 01:37:43 +0200

    refact after mentor

--- src/services/auth.js
+++ src/services/auth.js
@@ -52,9 +52,6 @@ export const loginUser = async (payload) => {
   if (!user) {
     throw createHttpError(401, 'Email or password invalid');
   }
-  if (!user.verify) {
-    throw createHttpError(401, 'Email not verified');
-  }
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
     throw createHttpError(401, 'Email or password invalid');
